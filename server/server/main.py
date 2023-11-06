f6e011ed3caa9fd81b2e2e823a1e674a606e63e5
hugeletters <ckuka71@gmail.com>
2023-11-06 17:25:43 +0100

    server example with schemas

--- server/server/main.py
+++ server/server/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI(root_path="/api/")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
-@app.get("/hello")
-async def hello():
-    return {"message": "Hello, hello"}
+class Hello(BaseModel):
+    Hello: str
+
+@app.get("/")
+def read_root() -> Hello:
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
