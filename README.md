d2e15e1d169465c69ec98b08af763230608c3bf3
hugeletters <ckuka71@gmail.com>
2023-11-11 07:59:55 +0100

    openapi gen instruction

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Team #12
+
 [Женя](https://t.me/HugeLetters), [Вадим](https://t.me/pnkvnky) и [Настя](https://t.me/palmette)
 
 # Stack
+
 ## Frontend
+
 - JS/TS
+- pnpm
 - Svelte
 - SvelteKit
 - Melt-UI
@@ -11,22 +15,33 @@
 - TanStack Query + Svelte-Query
 
 ## Backend
+
 - Python
 - FastAPI
 - SQLite
 
 ## Design
+
 [Figma](https://www.figma.com/file/14IWGKKD2T9XmvJ7IkBZXK/%D0%94%D0%B6%D1%83%D0%BD-%D1%85%D0%B0%D0%BA%D0%B0%D1%82%D0%BE%D0%BD%3A-%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D0%B0-%2312?type=design&node-id=0%3A1&mode=design&t=0GPo1EmlhcDVFA8D-1)
 
 # Project info
 
 # Deployment
+
 ## Local
+
 ```bash
 # either
 sudo docker compose up
-#or
+# or
 sudo bash run.sh
+
+
+# You should also run this command to generate types for openAPI spec for the client.
+cd client && pnpm run openapi
+# You can use other package managers to run this command but pnpm is recommended
 ```
+
 ## Hosted
+
 Hosted on Render.com - https://jun-hackathon.onrender.com/
--- client/scripts/openapi.ts
+++ client/scripts/openapi.ts
@@ -1,6 +1,6 @@
-import { readFile, writeFile } from 'node:fs/promises';
-import { fileURLToPath } from 'node:url';
-import generateOpenApiTypes, { type OpenAPI3 } from 'openapi-typescript';
+import { readFile, writeFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import generateOpenApiTypes, { type OpenAPI3 } from "openapi-typescript";
 
 generateOpenApi();
 async function generateOpenApi() {
@@ -8,18 +8,20 @@ async function generateOpenApi() {
 	if (!spec) return;
 
 	const types = await generateOpenApiTypes(spec);
-	writeFile('./src/lib/openapi.d.ts', types)
+	writeFile("./src/lib/openapi.d.ts", types)
 		.then(() => {
-			console.log('Sucessfully saved openapi.d.ts file');
+			console.log("Sucessfully saved openapi.d.ts file");
 		})
 		.catch((e) => {
 			console.error(e);
-			console.error('Error while trying to write openapi.d.ts file');
+			console.error("Error while trying to write openapi.d.ts file");
 		});
 }
 
 function getOpenApiSpec() {
-	return readFile(fileURLToPath(import.meta.resolve('../../server/openapi.json')), {
-		encoding: 'utf-8'
-	}).then<OpenAPI3>(JSON.parse);
+	return readFile(fileURLToPath(import.meta.resolve("../../server/openapi.json")), {
+		encoding: "utf-8",
+	})
+		.then<OpenAPI3>(JSON.parse)
+		.catch(console.error);
 }
