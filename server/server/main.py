1e7feb57287275d3e51af7269d5317afff16d7b3
VadimK <yaralavadim@gmail.com>
2023-11-11 17:09:57 +0200

    dotenv err fix

--- server/server/main.py
+++ server/server/main.py
@@ -3,6 +3,16 @@ from fastapi.openapi.utils import get_openapi
 from pydantic import BaseModel
 from typing import Union
 import json
+
+import os
+
+# python_path = os.environ.get("PYTHONPATH")
+#
+# if python_path:
+#     print(f"PYTHONPATH is set to: {python_path}")
+# else:
+#     print("PYTHONPATH is not set.")
+#
 # from db import create_canvas
 
 app = FastAPI(root_path="/api/")
@@ -33,10 +43,10 @@ def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
 
-# @app.post("/canvas/")
-# def create_canvas(background_color=None):
-#     canvas_id = create_canvas(background_color)
-#     return canvas_id
+@app.post("/canvas/")
+def create_canvas(background_color=None):
+    canvas_id = create_canvas(background_color)
+    return canvas_id
 
 
 # generate json spec file - required for client
