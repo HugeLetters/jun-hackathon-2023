df354073879d995ddf92057980cffc4026b1a31a
hugeletters <ckuka71@gmail.com>
2023-11-11 07:50:37 +0100

    cleanup

--- client/src/routes/+layout.ts
+++ client/src/routes/+layout.ts
@@ -1,22 +1,8 @@
-import { browser } from '$app/environment';
-import { api } from '$lib';
-import { QueryClient } from '@tanstack/svelte-query';
+import { browser } from "$app/environment";
+import { QueryClient } from "@tanstack/svelte-query";
 
 export async function load() {
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				enabled: browser
-			}
-		}
-	});
-
-	await queryClient.prefetchQuery({
-		queryKey: [['root', 'get']],
-		queryFn() {
-			return api.GET('/');
-		}
-	});
+	const queryClient = new QueryClient({ defaultOptions: { queries: { enabled: browser } } });
 
 	return { queryClient };
 }
