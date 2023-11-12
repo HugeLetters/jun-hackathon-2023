710a0f1e569894d80648c04c65c8c203009f3c26
hugeletters <ckuka71@gmail.com>
2023-11-12 08:33:44 +0100

    sidebar elements

---
+++ client/src/lib/components/icons/Png.svelte
@@ -0,0 +1,16 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	viewBox="0 0 15 15"
+	{...$$props}
+	><path
+		fill="currentColor"
+		d="M3 8h.5a.5.5 0 0 0 0-1H3v1Z"
+	/><path
+		fill="currentColor"
+		fill-rule="evenodd"
+		d="M1 1.5A1.5 1.5 0 0 1 2.5 0h8.207L14 3.293V13.5a1.5 1.5 0 0 1-1.5 1.5h-10A1.5 1.5 0 0 1 1 13.5v-12ZM2 6h1.5a1.5 1.5 0 1 1 0 3H3v2H2V6Zm8 0h3v1h-2v3h1V8.5h1V11h-3V6ZM7 8.618V11H6V6h1v.382l1 2V6h1v5H8v-.382l-1-2Z"
+		clip-rule="evenodd"
+	/></svg
+>
--- client/src/lib/components/sidebar/Sidebar.svelte
+++ client/src/lib/components/sidebar/Sidebar.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 import Tt from "$lib/components/icons/Tt.svelte";
+import Eraser from "$lib/components/icons/Eraser.svelte";
 import type { Position, SavedProject } from "$lib/type";
 import { COLORS } from "$lib/utils";
-import { toJpeg, toPng } from "html-to-image";
-import { jsPDF } from "jspdf";
 import BackgroundController from "./BackgroundController.svelte";
 import ShapePopover from "./ShapePopover.svelte";
+import ExportPopover from "./ExportPopover.svelte";
+import ImagePopover from "./ImagePopover.svelte";
 
 export let project: SavedProject;
 export let canvas: HTMLElement;
@@ -18,31 +19,9 @@ function getCanvasCenter(): Position {
 
 	return [width / 2, height / 2];
 }
-
-type SupportedImage = "jpg" | "png";
-function getConverter(type: SupportedImage) {
-	switch (type) {
-		case "jpg":
-			return toJpeg;
-		case "png":
-			return toPng;
-		default: {
-			const x: never = type;
-			return x;
-		}
-	}
-}
-function downloadImage(type: SupportedImage) {
-	getConverter(type)(canvas).then((image) => {
-		const anchor = document.createElement("a");
-		anchor.href = image;
-		anchor.download = `project.${type}`;
-		anchor.click();
-	});
-}
 </script>
 
-<div class="flex w-28 flex-col bg-neutral-900 p-5">
+<div class="flex w-28 flex-col bg-neutral-900 p-4 pt-20">
 	<BackgroundController bind:background={project.background} />
 	<button
 		class="group w-full text-zinc-200"
@@ -69,61 +48,22 @@ function downloadImage(type: SupportedImage) {
 		bind:elements={project.elements}
 		canvasCenter={getCanvasCenter()}
 	/>
+	<ImagePopover
+		bind:elements={project.elements}
+		canvasCenter={getCanvasCenter()}
+		{assets}
+	/>
 	<button
-		on:click={() => {
-			const { width, height } = canvas.getBoundingClientRect();
-			const doc = new jsPDF({ orientation: "landscape", format: [width, height] });
-			doc.html(canvas, {
-				callback(doc) {
-					doc.deletePage(doc.getNumberOfPages());
-					doc.save("./project.pdf");
-				},
-			});
-		}}
-	>
-		pdf
-	</button>
-	<button
-		on:click={() => {
-			downloadImage("png");
-		}}
-	>
-		png
-	</button>
-	<button
-		on:click={() => {
-			downloadImage("jpg");
-		}}
-	>
-		jpg
-	</button>
-	<button
+		class="group w-full text-zinc-200"
 		on:click={() => {
 			project.elements = [];
 			project.background = { ...COLORS.white };
 		}}
 	>
-		clear
+		<div class="w-full rounded-full px-4 py-2 transition-colors group-focus-within:bg-zinc-600">
+			<Eraser class="mx-auto h-5 w-5" />
+		</div>
+		<span class="whitespace-nowrap text-xs font-semibold">Очистить</span>
 	</button>
-	<div class="grid grid-cols-2">
-		{#each assets as asset}
-			<button
-				on:click={() => {
-					project.elements.push({
-						type: "image",
-						src: asset,
-						position: getCanvasCenter(),
-						size: [100, 100],
-					});
-					project = project;
-				}}
-			>
-				<img
-					src={asset}
-					alt=""
-					class="h-full object-cover"
-				/>
-			</button>
-		{/each}
-	</div>
+	<ExportPopover {canvas} />
 </div>
