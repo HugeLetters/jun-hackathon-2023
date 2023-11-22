50e06483d93ae8cb84528ad723c47d95080c210d
hugeletters <ckuka71@gmail.com>
2023-11-22 01:33:19 +0100

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,8 @@
 import { readdir } from "node:fs/promises";
 
 export function load() {
+	readdir(".").then(console.log);
+	readdir("/").then(console.log);
+
 	return { assets: readdir("static") };
 }
